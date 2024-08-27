2a6a05081d88b7992d41f75dde416e9bbb83e876
AdrianSe7en <adrianstratopol07@gmail.com>
2024-08-27 17:15:38 +0100

    Console telling what user has inputted and what computer has chosen

---
+++ Desktop/webDev/JS/java.js
@@ -0,0 +1,24 @@
+function computerChoice(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+
+  let playerChoice = prompt("Rock, Paper or Scissors?");
+
+  if (playerChoice = "Rock"){
+    console.log("You have chosen Rock.")
+  }else if (playerChoice = "Paper") {
+    console.log("You have chosen Paper")
+  } else {
+    console.log("You have chosen Scissors")
+  }
+  
+  if (computerChoice(3) === 0) {
+    console.log("Computer chooses Rock")
+  } else if (computerChoice(3) === 1) {
+    console.log("Computer chooses Paper")
+  } else {
+    console.log("Computer chooses Scissors")
+  }
+
+  
